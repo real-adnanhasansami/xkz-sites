caf6222aa13ec6ae38d0adfa229021faa68a2639
Adnan Hasan Sami <adnansite01@gmail.com>
2026-09-18 20:13:12 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,22 +4,23 @@ export function middleware(req) {
   const url = req.nextUrl;
   const hostname = req.headers.get('host') || '';
 
-  // আপনার মূল ডোমেইন বা লোকালহোস্ট হলে মিডলওয়্যার কাজ করবে না (হোমপেজ দেখাবে)
-  const currentHost = process.env.NODE_ENV === 'production' 
-    ? hostname.replace(`.vercel.app`, '') 
-    : hostname.replace(`.localhost:3000`, '');
+  // লোকালহোস্ট নাকি প্রোডাকশন ডোমেইন তা চেক করা
+  const currentHost = hostname
+    .replace('.vercel.app', '')
+    .replace('.localhost:3000', '');
 
-  // যদি মূল ডোমেইন বা www হয়, তবে রুট পেজে থাকতে দিন
+  // যদি মূল ডোমেইন, www বা লোকালহোস্ট হয়, তবে রুট পেজে থাকতে দিন
   if (
     currentHost === 'xkz' || 
     currentHost === 'localhost' || 
     currentHost === 'www' ||
-    !currentHost
+    !currentHost ||
+    currentHost.includes('127.0.0.1')
   ) {
     return NextResponse.next();
   }
 
-  // অন্যথায় সাবডোমেইন রিকোয়েস্টকে /sites/[subdomain]-এ রিরাইট করে দিন
+  // অন্যথায় সাবডোমেইন রিকোয়েস্টকে /sites/[subdomain]-এ রিরাইট করুন
   return NextResponse.rewrite(new URL(`/sites/${currentHost}${url.pathname}`, req.url));
 }
 
